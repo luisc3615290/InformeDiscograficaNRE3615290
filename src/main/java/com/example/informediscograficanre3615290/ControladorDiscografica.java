1c67a7acb3dce0c05513e282be1e902878e346da
luisc3615290 <3615290@alu.murciaeduca.es>
2025-02-25 04:19:34 +0100

    25/02 > Cambio de default branch

--- src/main/java/com/example/informediscograficanre3615290/ControladorDiscografica.java
+++ src/main/java/com/example/informediscograficanre3615290/ControladorDiscografica.java
@@ -3,7 +3,7 @@ package com.example.informediscograficanre3615290;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ControladorDiscografica {
     @FXML
     private Label welcomeText;
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.informediscograficanre3615290 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jasperreports;
+    requires java.sql;
 
 
     opens com.example.informediscograficanre3615290 to javafx.fxml;
