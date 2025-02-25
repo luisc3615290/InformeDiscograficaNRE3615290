a72a61652b13f5994c5ddb7f651b1433b2022eef
luisc3615290 <3615290@alu.murciaeduca.es>
2025-02-25 04:02:28 +0100

    25/02 > Kickoff proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.informediscograficanre3615290 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.informediscograficanre3615290 to javafx.fxml;
+    exports com.example.informediscograficanre3615290;
+}
